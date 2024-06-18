8324516e023316ea168b58c12662707357187ff5
Alysson Demari <145402961+AlyssonDemari@users.noreply.github.com>
2024-06-18 09:32:54 -0300

    teste
     teste

---
+++ teste rapido/dia da semana/app.js
@@ -0,0 +1,5 @@
+let dia = prompt("Que dia é hoje?");
+if (dia === "sábado" || dia ==="sabado" || dia === "domingo") {
+    alert("Tenha um ótimo final de semana");
+} elsesegunda 
+ alert("Boa semana!")
