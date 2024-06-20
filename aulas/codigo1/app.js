c9a2a42994ba1e7ce294e5907a81d5896335edd5
Alysson Demari <alyssondemari@gmail.com>
2024-06-19 22:25:53 -0300

    aula de lógica

--- aulas/codigo1/app.js
+++ aulas/codigo1/app.js
@@ -1,17 +1,20 @@
 alert ("Boas-Vindas ao jogo do número secreto");
 let numeroSecreto = 2; 
-console.log(numeroSecreto)
-let chute 
+console.log(numeroSecreto);
+let chute;
+let tentativas = 1;
 
 while ( chute != numeroSecreto) {
     chute = prompt ("Escolha um número de 1 a 10");
     if (chute == numeroSecreto){
-        alert(`Você descobriu o número secreto ${numeroSecreto}`);
+        alert(`Você descobriu o número secreto "${numeroSecreto}" com apenas ${tentativas} tentativa(s) `);
     } else {
         if (chute > numeroSecreto) {
             alert(`O número secreto é menor que ${chute}`);
         } else {
-            alert(`O número secreto é maior que ${chute}`)
+            alert(`O número secreto é maior que ${chute}`);
         }
+        // tentativas = tentativas + 1
+        tentativas++;
     }
 }
---
+++ desafio rapido/contador/app.js
@@ -0,0 +1,37 @@
+//desafio 1
+
+// let contador = 0;
+// while (contador < 10) {
+//     contador++
+//     alert(`${contador}`);
+// }
+
+
+//desafio 2
+
+// let contador = 11;
+// while (contador > 0) {
+//     contador--
+//     alert(`${contador}`);
+// }
+
+
+//desafio 3
+
+// let cont = prompt("Desseja fazer uma contagem regressiva a partit de qual número?");
+// while (cont >= 1) {
+//     contador = cont
+//     alert(`${cont}`);
+//     console.log(contador);
+//     cont--;
+// }
+
+
+//desafio 4
+
+let contador = 0
+let cont = prompt("Até qual número você quer contar?");
+while(contador < cont){
+    contador++
+    alert(`${contador}`);
+}
