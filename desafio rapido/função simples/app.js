c68be8e9aba51186e5d1d081014446681ba5fe88
Alysson Demari <145402961+AlyssonDemari@users.noreply.github.com>
2024-06-25 14:34:20 -0300

    testando funções

---
+++ desafio rapido/função simples/app.js
@@ -0,0 +1,26 @@
+console.log("Olá, Mundo!"); 
+
+
+function exibirNome(nome){
+    console.log(`Olá, ${nome}`);
+}
+
+exibirNome("Alice");
+
+function calcularDobro(numero) {
+    return numero * 2;
+  }
+  
+  let resultadoDobro = calcularDobro(5);
+  console.log(resultadoDobro);
+
+  function calculoMedia(media){
+
+  }
+
+  function calcularMedia(a, b, c) {
+    return (a + b + c) / 3;
+  }
+  
+  let media = calcularMedia(4, 7, 10);
+  console.log(media);
